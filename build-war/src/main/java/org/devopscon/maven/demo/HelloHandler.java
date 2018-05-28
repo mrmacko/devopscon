4ec0e7b1ca510770b8a28c46421d7f39ab4c4838
mrmacko <37367244+mrmacko@users.noreply.github.com>
2018-05-28 16:23:20 +0200

    Update HelloHandler.java
    another blabla

--- build-war/src/main/java/org/devopscon/maven/demo/HelloHandler.java
+++ build-war/src/main/java/org/devopscon/maven/demo/HelloHandler.java
@@ -3,7 +3,7 @@ package org.devopscon.maven.demo;
 public class HelloHandler {
 
 	public String sayHello() {
-		String hello = "Hi man? What do you think about this WAR file? This is awesome!";
+		String hello = "Hi man? What do you think about this WAR file? This is awesome! And the Pipeline too";
 		return hello;
 	}
 
